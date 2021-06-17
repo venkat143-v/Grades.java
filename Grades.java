9db74e4bdad52798e9d5d4b2cc9cc3d5a72f8857
venkat143-v <81428675+venkat143-v@users.noreply.github.com>
2021-06-17 17:05:52 +0530

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,15 @@
+public class Grades {
+	public static void main(String[] args) {
+		int marks = 70;
+		if(marks <35) 
+			System.out.println("Fail");
+		else if(marks >=35 && marks < 50)
+			System.out.println("C Grade");
+		else if(marks >=50 && marks < 60)
+			System.out.println("B Grade");
+		else if(marks >=60)
+			System.out.println("A Grade");
+		else
+			System.out.println("Invalid");		
+	}
+}
